0e3d52f14e197fe3dd65dd8c263bc115bb5a3fad
Mohd Ahmer Khan <mohd.ahmerkhan@nissanmotor.com>
2023-01-04 14:44:58 +0530

    Updated Server On Message

--- src/main/java/com/nissan/AppreciationAppApplication.java
+++ src/main/java/com/nissan/AppreciationAppApplication.java
@@ -8,7 +8,7 @@ public class AppreciationAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppreciationAppApplication.class, args);
-		System.out.println("Running Aprreciation App - REST API");
+		System.out.println("Running Aprreciation App BackEnd - REST API");
 	}
 
 }
